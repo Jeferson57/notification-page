437622109333b452f807843f7cb4d44bb8cea02b
Jeferson Mejicanos <alexandermejicanos57@gmail.com>
2024-04-01 23:44:11 -0600

    Add javascript the page

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const markAll = document.querySelector('#mark-all');
+const numberIndeticator = document.querySelector('#number');
+const pots = document.querySelectorAll('.post');
+console.log(pots);
+
+pots.forEach(post => {
+    post.addEventListener('click', () => {
+        post.querySelector('.not-read').classList.remove('not-read');
+        updateNotification();
+    })
+})
 
 markAll.addEventListener('click', () => {
     const notReadElements = document.querySelectorAll('.not-read');
+    
+
     notReadElements.forEach((notReadElements) => {
         notReadElements.classList.remove('not-read');
     })
+
+    updateNotification(); 
 });
+
+const updateNotification = () => {
+    const notReadActual = document.querySelectorAll('.not-read');
+    numberIndeticator.innerText = notReadActual.length;
+}
